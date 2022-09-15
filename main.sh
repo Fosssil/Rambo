bec79a8c0f4d1beb709d711ad3183485c41c33e5
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-09-15 14:33:57 +0530

    initial code 0.1.1

--- main.sh
+++ main.sh
@@ -13,17 +13,17 @@ tput setaf 7
 
 ## Code to check recommended packages in system
 echo "Checking for required packages..."
-pack=("rsync" "ssh" "bnm")
+pack=("rsync" "ssh" "bnm" "tar" "gzip" "bunzip" "bzip2")
 for pkg in "${pack[@]}";
 do
 if [ "$(command -v "$pkg")" ]; 
 then
     tput setaf 2
-    echo "command ""$pkg"" exists on system"
+    echo "$pkg"" exists on system"
     tput setaf 7
 else
 tput setaf 1
-echo "$pkg"" Not exist"
+echo "$pkg"" do not exists"
 tput setaf 7
 fi
 done
