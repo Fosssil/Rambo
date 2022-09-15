196f18e3eef8c9c5646541805896b3f0fd9aa793
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-09-15 18:03:23 +0530

    major changes in code

--- main.sh
+++ main.sh
@@ -13,14 +13,14 @@ echo "
   ███    ███                                                       
 "
 tput setaf 5
-echo " A application in development to Backup and Restore files"
+echo "A application in development to Backup and Restore files"
 echo ""
 tput setaf 7
 
 ## Code to check recommended packages in system
 echo "Checking for required packages..."
 pack=("rsync" "ssh" "bnm" "tar" "gzip" "bunzip" "bzip2")
-for pkg in "${pack[@]}";
+for pkg in "${pack[@]}"; 
 do
 if [ "$(command -v "$pkg")" ]; 
 then
@@ -32,7 +32,7 @@ pack_to_inst+=("$pkg")
 fi
 done
 for pkg_inst in "${pack_to_inst[@]}";
-do
+ do
     tput setaf 1
     echo "$pkg_inst" "not available"
     tput setaf 7
