f323314d89749ee18d76c346098644d175f5a56f
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-09-16 01:07:52 +0530

    initial code 0.1.2

--- main.sh
+++ main.sh
@@ -19,21 +19,70 @@ tput setaf 7
 
 ## Code to check recommended packages in system
 echo "Checking for required packages..."
-pack=("rsync" "ssh" "bnm" "tar" "gzip" "bunzip" "bzip2")
+pack=("rsync" "hiihi" "ssh" "tar" "jji" "gzip" "bunzip" "bzip2")
+echo ""
+tput bold setaf 2
+echo "Installed on system :"
 for pkg in "${pack[@]}"; 
 do
-if [ "$(command -v "$pkg")" ]; 
+if [ "$(command -v "$pkg")" ];  # packages available
 then
-    tput setaf 2
-    echo "$pkg"" exists on system" 
+    tput bold setaf 2
+    echo "+" "$pkg" 
+    tput sgr0
     tput setaf 7
 else
 pack_to_inst+=("$pkg")
 fi
 done
-for pkg_inst in "${pack_to_inst[@]}";
- do
-    tput setaf 1
-    echo "$pkg_inst" "not available"
-    tput setaf 7
+function inst {
+    for pkg_inst in "${pack_to_inst[@]}"; # packages not available
+    do
+    echo "$pkg_inst";
+    done
+}
+result=$(inst)
+echo ""
+tput bold setaf 1
+echo "NOT Installed :"
+tput blink 
+echo "$result"
+tput sgr0 setaf 7
+echo ""
+
+## Code to install essential packages
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+for f in "${!osInfo[@]}"
+do
+    if [[ -f $f ]];then
+        echo Package manager: ${osInfo[$f]}
+    fi
 done
+
+packagesNeeded=$(inst)
+if [ -x "$(command -v apk)" ]
+then
+sudo apk add --no-cache "$packagesNeeded"
+elif [ -x "$(command -v pacman)" ];
+then
+sudo pacman -S "$packagesNeeded"
+elif [ -x "$(command -v apt-get)" ];  # reminder: update to APT
+then
+sudo apt-get install "$packagesNeeded"
+elif [ -x "$(command -v dnf)" ];
+then
+sudo dnf install "$packagesNeeded"
+elif [ -x "$(command -v zypper)" ]; 
+then sudo zypper install "$packagesNeeded"
+else
+tput setaf 1
+echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install:"
+tput setaf 7
+echo "$packagesNeeded">&2;
+fi
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 declare -A osInfo;
 osInfo[/etc/redhat-release]=yum
 osInfo[/etc/arch-release]=pacman
@@ -6,10 +7,34 @@ osInfo[/etc/gentoo-release]=emerge
 osInfo[/etc/SuSE-release]=zypp
 osInfo[/etc/debian_version]=apt-get
 osInfo[/etc/alpine-release]=apk
-
 for f in "${!osInfo[@]}"
 do
     if [[ -f $f ]];then
         echo Package manager: ${osInfo[$f]}
     fi
 done
+
+#packagesNeeded=$pkg_inst
+#if [ -x "$(command -v apk)" ]
+#then
+#sudo apk add --no-cache "$packagesNeeded"
+#elif [ -x "$(command -v pacman)" ];
+#then
+#sudo pacman -S "$packagesNeeded"
+#elif [ -x "$(command -v apt-get)" ];
+#then
+#sudo apt-get install "$packagesNeeded"
+#elif [ -x "$(command -v dnf)" ];
+#then
+#sudo dnf install "$packagesNeeded"
+#elif [ -x "$(command -v zypper)" ]; 
+#then sudo zypper install "$packagesNeeded"
+#else 
+#echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install:
+#$packagesNeeded">&2;
+#fi
+test_function() {
+echo Test
+}
+result=$(test_function)
+echo "$result" is saved in a variable for later use
