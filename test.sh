afe45855093411a5023d098ab249c24b389b8bd0
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-09-20 01:30:24 +0530

    :(

--- test.sh
+++ test.sh
@@ -18,6 +18,17 @@ source inputfile.sh
 
 #code to find package manager >>>>>>
 
+FILE=$(zenity --file-selection --multiple --title="Select a File")
+
+case $? in
+         0)
+                echo "\"$FILE\" selected.";;
+         1)
+                echo "No file selected.";;
+        -1)
+                echo "An unexpected error has occurred.";;
+esac
+
 #declare -A osInfo;
 #osInfo[/etc/redhat-release]=yum
 #osInfo[/etc/arch-release]=pacman
