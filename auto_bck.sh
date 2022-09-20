36243432cc8ff1ed2e7101d5b1027d26fc1fecc9
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-09-20 14:35:15 +0530

    fixed bugs and ui elements

--- auto_bck.sh
+++ auto_bck.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/bash
 destination=test/ # Variable: store destination address where, backup files will be placed
 target=(
-    /home/"$USER"/.bash_history
-    /home/"$USER"/.bashrc
-    /home/"$USER"/.ssh
-    /home/"$USER"/.npm
-    /home/"$USER"/Documents
-    /home/"$USER"/.zshrc
-    /home/"$USER"/.local/share/kwalletd
-    /home/"$USER"/.local/share/kwin
-    /home/"$USER"/.zsh_history
-    /home/"$USER"/.local/share/plasma
-    /home/"$USER"/.local/share/konsole
-    /home/"$USER"/token.txt
+    "/home/$USER/.bash_history"
+    "/home/$USER/.bashrc"
+    "/home/$USER/.ssh"
+    "/home/$USER/.npm"
+    "/home/$USER/.zshrc"
+    "/home/$USER/.zsh_history"
+    "/home/$USER/.local/share/kwalletd"
+    "/home/$USER/.local/share/kwin"
+    "/home/$USER/.local/share/plasma"
+    "/home/$USER/.local/share/konsole"
+    "/home/$USER/.config/autostart/"
+    "/home/$USER/.config/Code - OSS/User/settings.json"
+    "/home/$USER/.config/VSCodium/User/settings.json"
+    "/home/$USER/.config/kwalletrc"
+    "/home/$USER/.config/latte"
+    "/home/$USER/token.txt"
+    "/home/$USER/Documents"
 ) # Array: store targated locations
-COUNTER=1       # Variable: counter used in below for loop
+
+COUNTER=1 # Variable: counter used in below for loop
 echo -e "\e[35mFollowing files are going to be backed up :) \e[0m"
-for i in "${target[@]}"; do     # Loop: (for) to display the array items to user .i.e. target location
+for i in "${target[@]}"; do # Loop: (for) to display the array items to user .i.e. target location
     echo -e "\e[33m" "$COUNTER." "$i" "\e[0m"
     COUNTER=$((COUNTER + 1))
 done
 
 # code for rsync
 while true; do                                     # Loop: (while) to get user input to proceed to further steps or not
-    read -r -p "Do you want to proceed? (y/N) " yn # Command: read user input
+    read -r -p "Do you want to proceed? (y/n) " yn # Command: read user input
     case $yn in                                    # Conditon: switch case
     [yY])
         echo -e "\e[32m ok, we will do stuff \e[0m" # Input: for yes
