95d6723391e2b2b35da1d008e8d1b9a89997149b
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-09-21 17:50:27 +0530

    added backup code

--- bck.sh
+++ bck.sh
@@ -7,24 +7,26 @@ dialog --begin 10 30 --backtitle "Rambo by fossil" \
     * Please Do not leave it empty.
     * Empty line end the taking input.
 
-      Thanks' 20 50
+      Thanks' 20 70
 clear
-#
-#dialog --begin 10 30 --backtitle "System Information" \
-#    --title "About" \
-#    --msgbox 'This is an entirely open source software.' 10 30
-#sleep 1s
-#clear
+
+dialog --begin 10 30 --backtitle "Rambo by fossil" \
+    --title "!!! CAUTION !!!" \
+    --msgbox 'Some files from System Root(.i.e. / ) can not be successfully backup.
+    They may show error with permission, specially private key files.
+    Will fix someday.
+    Goodluck :) ' 20 70
+clear
 
 echo -e "\e[1;32mEnter your files location to take backup: \e[0m"
 echo -e "\e[31m(end with an empty line)\e[0m"
 array=() # Array: to store files location
+destination=test/
 
 while IFS= read -r line; do # Loop: (while) to enter data from user into array
     [[ $line ]] || break    # break if line is empty
     array+=("$line")
 done
-
 if [ "${#array[@]}" -eq 0 ]; then # Condition: (if) array is empty
     echo "Please enter something..."
 else
@@ -33,4 +35,5 @@ else
     for list in "${array[@]}"; do
         echo -e "\e[33m$list\e[0m"
     done
+    rsync -Rrnv --progress "${array[@]}" "$destination" # Command: rsync to backup files, -n to dry_run
 fi
