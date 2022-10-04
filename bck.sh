e49f60601c65c5b63cf5864318c988647705721b
Fosssil <dahiyabadshah.fossil@gmail.com>
2022-10-05 03:17:13 +0530

    added new code for new UI

--- bck.sh
+++ bck.sh
@@ -18,22 +18,27 @@ dialog --begin 10 30 --backtitle "Rambo by fossil" \
     Goodluck :) ' 20 70
 clear
 
-echo -e "\e[1;32mEnter your files location to take backup: \e[0m"
-echo -e "\e[31m(end with an empty line)\e[0m"
-target=() # Array: to store files location
+#echo -e "\e[1;32mEnter your files location to take backup: \e[0m"
+#echo -e "\e[31m(end with an empty line)\e[0m"
 destination=test/
+cd /
+declare -a target
+readarray -t target < <(fzf -m --scheme=path \
+    --tiebreak=begin,index \
+    --border=rounded --ansi \
+    --marker='+' --prompt='/' \
+    --header='Select files' --preview='tree -C -L 3 -x')
 
-while IFS= read -r line; do # Loop: (while) to enter data from user into array
-    [[ $line ]] || break    # break if line is empty
-    target+=("$line")
-done
+
+
 if [ "${#target[@]}" -eq 0 ]; then # Condition: (if) array is empty
     echo "Please enter something..."
 else
-COUNTER=1 # Variable: counter used in below for loop
-    echo -e "\e[32mFollowing files will be backup: \e[0m" # Condition: (else) array have locations
-    for list in "${target[@]}"; do
-        echo -e "\e[33m$COUNTER. $list\e[0m"
-    done
-    rsync -Rrnv --progress "${target[@]}" "$destination" # Command: rsync to backup files, -n to dry_run
+COUNTER=1                                             # Variable: counter used in below for loop
+echo -e "\e[32mFollowing files will be backup: \e[0m" # Condition: (else) array have locations
+for list in "${target[@]}"; do
+    echo -e "\e[33m$COUNTER. $list\e[0m"
+        COUNTER=$((COUNTER + 1))
+done
+rsync -Rrnv --progress "${target[@]}" "$destination" # Command: rsync to backup files, -n to dry_run
 fi
